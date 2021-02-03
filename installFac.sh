7bd5467c6a908e862e703860f1ac3ee8a972fa24
Kagari <aymeric.adenot27@orange.fr>
2021-02-03 12:00:41 +0100

    Modification du fichier installFac pour pouvoir l'adapter facilement sur d'autre projet du même type

--- installFac.sh
+++ installFac.sh
@@ -1,8 +1,18 @@
 #!/bin/bash 
-cp note.sh $HOME/.note.sh
 
-if [ ! $(grep -q "alias note=" $HOME/.bashrc) ];then
-   echo "alias note='~/.note.sh'" >> $HOME/.bashrc
+#Variable qui définit le script à prendre en compte
+script='note.sh'
+
+#La varibale script et découpe puis on retire l'extensions
+alias=$(echo $script | cut -f 1 -d .)
+
+#Copie du script dans le home
+cp ${script} $HOME/${script}
+
+if [ ! $(grep -q "alias ${alias}=" $HOME/.bashrc) ];then
+   #Si l'alias n'est pas present on le construit
+   #Puis l'alias est ajoute dans le bashrc
+   echo -e "alias ${alias}='~/.${script}'" >> $HOME/.bashrc
 fi
 
 echo "Redémarrer le terminal ^^ "
