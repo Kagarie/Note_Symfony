3c13eeba38a63fb4916c39ab3a2e3422aeda437f
aymeric <aymeric.adenot27@orange.fr>
2022-12-20 13:24:24 +0100

    changement

--- uninstall.sh
+++ uninstall.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+SCRIPT=noteSymfony
+
 function del() {
     rc="rc"
     if [ -f "$HOME/.$1$rc" ]; then
         echo "Suppression de l'alias du $1$rc"
-        sed -i -e '/noteSymfony/d' $HOME/.$1$rc
+        sed -i -e "/$SCRIPT/d" $HOME/.$1$rc
     fi
 }
 
 del "bash"
 del "zsh"
 
-if [ -f "$HOME/.noteSymfony.sh" ]; then
+if [ -f "$HOME/.$SCRIPT.sh" ]; then
     echo 'Suppresion du script'
 
-    rm "$HOME/.noteSymfony.sh"
+    rm "$HOME/.$SCRIPT.sh"
 fi
 
 echo "Uninstall done"
